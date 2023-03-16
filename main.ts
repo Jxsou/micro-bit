adcbf6c175018797feca71f6e1174c9f56eb0e81
Jxsou <100725050+Jxsou@users.noreply.github.com>
2023-03-16 16:25:43 -0400

    startup

--- main.ts
+++ main.ts
@@ -1,3 +1,19 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    led.plot(x - 1, 4)
+    led.unplot(x, 4)
+    x += -1
 })
+input.onButtonPressed(Button.B, function () {
+    led.plot(x + 1, 4)
+    led.unplot(x, 4)
+    x += 1
+})
+let x = 0
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . # . .
+    `)
+x = 2
