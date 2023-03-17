ce85c362804abc2eb62866b4a206c56c8a4d7fc8
Jxsou <100725050+Jxsou@users.noreply.github.com>
2023-03-17 19:37:17 -0400

    finished project

--- main.ts
+++ main.ts
@@ -1,13 +1,29 @@
 input.onButtonPressed(Button.A, function () {
-    led.plot(x - 1, 4)
-    led.unplot(x, 4)
-    x += -1
+    if (x >= 1) {
+        led.plot(x - 1, 4)
+        led.unplot(x, 4)
+        x += -1
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    shootingPosition = x
+    for (let index = 0; index < 4; index++) {
+        led.plot(shootingPosition, y - 1)
+        y += -1
+        basic.pause(100)
+        led.unplot(shootingPosition, y)
+    }
+    y = 4
 })
 input.onButtonPressed(Button.B, function () {
-    led.plot(x + 1, 4)
-    led.unplot(x, 4)
-    x += 1
+    if (x <= 3) {
+        led.plot(x + 1, 4)
+        led.unplot(x, 4)
+        x += 1
+    }
 })
+let shootingPosition = 0
+let y = 0
 let x = 0
 basic.showLeds(`
     . . . . .
@@ -17,3 +33,7 @@ basic.showLeds(`
     . . # . .
     `)
 x = 2
+y = 4
+loops.everyInterval(2500, function () {
+    led.plot(randint(0, 4), 0)
+})
